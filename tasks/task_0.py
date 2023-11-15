7bcdebd68509bce1ad11a01c106fb8beb7b3845a
Dasha Demidova <striki06@mail.ru>
2023-11-15 13:00:36 +0300

    done

--- tasks/task_0.py
+++ tasks/task_0.py
@@ -1,1 +1,20 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+# программа, которая считывает целые числа с клавиатуры до тех пор,
+# пока не будет введено три отрицательных числа
+
+count = 0
+summ_positive = 0
+summ_negative = 0
+
+while True:
+    if count == 3:
+        break
+    else:
+        number = int(input())
+        if number <= 0:
+            count += 1
+            summ_negative += number
+        else:
+            summ_positive += number
+
+print('Сумма положительных чисел:', summ_positive)
+print('Сумма отрицательных чисел:', summ_negative)
